38dfcf60ec3ac750fceb80690de917c6cb28929d
Ariel <ariel.kuzminski@gmail.com>
2017-01-30 20:07:10 +0100

    pizzaClicker 0.0.3

--- PizzaClicker/js/app.js
+++ PizzaClicker/js/app.js
@@ -4,8 +4,9 @@ function PizzaClickerGame() {
     var autoClick = 0;                                          // licznik automatycznych kliknięć na sekundę
     
     
-    upgrades = [                                                // każde z ulepszeń
+    var upgrades = [                                                // każde z ulepszeń
         {
+            name : 'pizzerman',
             id : '#pizzerman',                                  //aby móc akutalizować wyniki na stronie
             idValue : '#pizzerman-value',                       // j.w
             idCount : '#pizzaViewCount',                        // do wzoru na koszt kolejnego ulepszenia
@@ -16,6 +17,7 @@ function PizzaClickerGame() {
         },
         
         {
+            name : 'waitress',
             id : '#waitress',
             idValue : '#waitress-value',
             idCount : '#waitressViewCount',
@@ -23,21 +25,54 @@ function PizzaClickerGame() {
             currentPrice : 100,
             basePrice: 100,
             bonus : 10
+        },
+        
+        {
+            name : 'deliveryboy',
+            id : '#deliveryboy',
+            idValue : '#deliveryboy-value',
+            idCount : '#deliveryboyViewCount',
+            itemCount : 0,
+            currentPrice : 1000,
+            basePrice: 1000,
+            bonus : 100
+        },
+        
+        {
+            name : 'manager',
+            id : '#manager',
+            idValue : '#manager-value',
+            idCount : '#managerViewCount',
+            itemCount : 0,
+            currentPrice : 10000,
+            basePrice: 10000,
+            bonus : 1000
+        },
+        
+        {
+            name : 'sszef',
+            id : '#manager',
+            idValue : '#manager-value',
+            idCount : '#managerViewCount',
+            itemCount : 0,
+            currentPrice : 10000,
+            basePrice: 10000,
+            bonus : 1000
         }
     ];
     
 
-    this.clickClicker = function() {                            //kliknięcie w clicker...
+    var clickClicker = function() {                            //kliknięcie w clicker...
         
         pizzaCount++;
-        this.update();
+        update();
         
     };
     
     
-    this.clickUpgrade = function(currentUpgrade) {              //kliknięcie w jakiekolwiek ulepszenie
+    var clickUpgrade = function(currentUpgrade) {              //kliknięcie w jakiekolwiek ulepszenie
         
-        this.update();                                          //wywołanie funkcji aktualizującej wyświetlanie danych na stronie
+        update();                                          //wywołanie funkcji aktualizującej wyświetlanie danych na stronie
         
         
             
@@ -48,7 +83,7 @@ function PizzaClickerGame() {
             currentUpgrade.currentPrice = (currentUpgrade.basePrice * (Math.pow(1.15, currentUpgrade.itemCount)));  // Pani Partyka byłaby ze mnie dumna...
             currentUpgrade.currentPrice = Math.round(currentUpgrade.currentPrice);                                  // żeby był int a nie float
             
-            this.update();
+            update();
         }
         
     };
@@ -66,7 +101,7 @@ function PizzaClickerGame() {
 //        
 //    };
     
-    this.checkPizza = function() {                              // metoda sprawdzająca czy można się wklikać w dane ulepszenie czy nie
+    var checkPizza = function() {                              // metoda sprawdzająca czy można się wklikać w dane ulepszenie czy nie
         
         for(var i = 0; i < upgrades.length; i++) {
             
@@ -80,7 +115,7 @@ function PizzaClickerGame() {
         }
     };
 
-    this.update = function() {                                      // metoda często wywoływana - odpowiedzialna za aktualizacje danych wyświetlanych na stronie
+    var update = function() {                                      // metoda często wywoływana - odpowiedzialna za aktualizacje danych wyświetlanych na stronie
         $('title').text(pizzaCount + ' pizzas!');
         $('#counter').text(pizzaCount);
         
@@ -94,41 +129,38 @@ function PizzaClickerGame() {
     
     ////////@@@@@@@@@@@@@@@@@@///////// TO DO ////////////@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@///////////////////
     
-    this.updateAutoClicks = function() { // do poprawki: metoda odpowiedzialna za dodanie automatycznych kliknięć do całkowitej liczby pizz - w razie zmiany SetInterval z 1 sekundy, wszystko zacznya źle działać :(
+    var updateAutoClicks = function() { // do poprawki: metoda odpowiedzialna za dodanie automatycznych kliknięć do całkowitej liczby pizz - w razie zmiany SetInterval z 1 sekundy, wszystko zacznya źle działać :(
         pizzaCount = pizzaCount + autoClick;
     };
 
     ////////@@@@@@@@@@@@@@@@@@///////// TO DO ////////////@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@///////////////////
 
-    this.save = function() {                                        //metoda zapisująca postęp gry do localStorage
+    var save = function() {                                        //metoda zapisująca postęp gry do localStorage
         localStorage.setItem('pizzaCount', pizzaCount);
         localStorage.setItem('autoClick', autoClick);
         
-        localStorage.setItem('pizzermanCost', upgrades[0].currentPrice);
-        localStorage.setItem('pizzermanCount', upgrades[0].itemCount);
-        
-        localStorage.setItem('waitressCost', upgrades[1].currentPrice);
-        localStorage.setItem('waitressCount', upgrades[1].itemCount);
+        for(var i = 0; i < upgrades.length; i++) {
+            localStorage.setItem(upgrades[i].name + 'Cost', upgrades[i].currentPrice);
+            localStorage.setItem(upgrades[i].name + 'Count', upgrades[i].itemCount);
+        }
         console.log('zapisano');
 
     };
 
-    this.load = function() {                                        //metoda wczytująca postęp gry z localStorage
+    var load = function() {                                        //metoda wczytująca postęp gry z localStorage
         
-        //this.update();
+        update();
         
         if(localStorage.getItem('pizzaCount')) {                    // jeżeli istnieje już jakakolwiek pizza to wczytaj dane...
             
-            upgrades[0].currentPrice = localStorage.getItem('pizzermanCost')
-            upgrades[0].currentPrice = parseInt(upgrades[0].currentPrice);
-            upgrades[0].itemCount = localStorage.getItem('pizzermanCount')
-            upgrades[0].itemCount = parseInt(upgrades[0].itemCount);
             
-            
-            upgrades[1].currentPrice = localStorage.getItem('waitressCost')
-            upgrades[1].currentPrice = parseInt(upgrades[1].currentPrice);
-            upgrades[1].itemCount = localStorage.getItem('waitressCount')
-            upgrades[1].itemCount = parseInt(upgrades[1].itemCount);
+            for(var i = 0; i < upgrades.length; i++) {
+                
+                upgrades[i].currentPrice = localStorage.getItem(upgrades[i].name + 'Cost');
+                upgrades[i].currentPrice = parseInt(upgrades[i].currentPrice);
+                upgrades[i].itemCount = localStorage.getItem(upgrades[i].name + 'Count');
+                upgrades[i].itemCount = parseInt(upgrades[i].itemCount);
+            }
             
             pizzaCount = localStorage.getItem('pizzaCount');
             pizzaCount = parseInt(pizzaCount);
@@ -138,69 +170,80 @@ function PizzaClickerGame() {
             
            }
         else {                                                      // ... w przeciwnym wypadku zresetuj wynik
-            this.reset();
+            reset();
         };
     
-        this.update();
+        update();
     };
     
-    this.reset = function() {                                       // metoda resetująca grę
-        localStorage.setItem('pizzaCount', 0);
-        localStorage.setItem('autoClick', 0);
-        localStorage.setItem('pizzermanCost', upgrades[0].basePrice);
-        localStorage.setItem('waitressCost', upgrades[1].basePrice);
-        localStorage.setItem('pizzermanCount', 0);
-        localStorage.setItem('waitressCount', 0);
+    var reset = function() {                                       // metoda resetująca grę
+        
+        for(var i = 0; i < upgrades.length; i++) {
+            
+            localStorage.setItem('pizzaCount', 0);
+            localStorage.setItem('autoClick', 0);
+            
+            localStorage.setItem(upgrades[i].name + 'Cost', upgrades[i].basePrice);
+            localStorage.setItem(upgrades[i].name + 'Count', 0);
+            
+        }
     }
     
-    this.construct = function() {                                   // konstruktor
-        this.load();                                                // wczytywanie danych z localStorage po wejściu na stronę
-        setInterval(this.update, 1000);                             // wywołanie metody aktualizującej dane na stronie co 1 sekundę
-        setInterval(this.checkPizza, 100);                          // wywołanie metody sprawdzającej czy można się wklikać w dane ulepszenie czy nie co 0.1s
-        setInterval(this.save, 60000);                              // autozapis co 60 sekund
-        setInterval(this.updateAutoClicks, 1000);                   // TO DO // nie wolno zmieniać wartości, inaczej wszystko się psuje :( :( :(
+    var events = function() {
+
+//        var ev = this;
+        
+        $( document ).on( 'click', '#clicker', clickClicker );
+
+        $( document ).on( 'click', '#pizzerman', function() {
+            clickUpgrade(upgrades[0]);
+        } );
+        
+        $( document ).on( 'click', '#waitress', function() {
+            clickUpgrade(upgrades[1]);
+        } );
+
+        $( document ).on( 'click', '#deliveryboy', function() {
+            clickUpgrade(upgrades[2]);
+        } );
+
+        $( document ).on( 'click', '#manager', function() {
+            clickUpgrade(upgrades[3]);
+        } );
+
+        $( document ).on( 'click', '#save', save );
+
+        $( document ).on( 'click', '#load', load );
+
+        $( document ).on( 'click', '#reset', function() {
+            reset();
+            load();
+        });
+        
+    };
+    
+    
+    
+    
+    var construct = function() {                                   // konstruktor
+        events();
+        load();                                                // wczytywanie danych z localStorage po wejściu na stronę
+        setInterval(update, 1);                             // wywołanie metody aktualizującej dane na stronie co 1 sekundę
+        setInterval(checkPizza, 100);                          // wywołanie metody sprawdzającej czy można się wklikać w dane ulepszenie czy nie co 0.1s
+        setInterval(save, 60000);                              // autozapis co 60 sekund
+        setInterval(updateAutoClicks, 1000);                   // TO DO // nie wolno zmieniać wartości, inaczej wszystko się psuje :( :( :(
         
         $('#pizzerman').attr("disabled", true);                     // buttony domyślnie ustawione na disabled
         
     }
     
-    this.construct();
+    return { start: construct };
+    
 
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-var Clicker = new PizzaClickerGame();
-
-
-$('#clicker').on('click', function(){
-    Clicker.clickClicker();
-    
-});
-
-$('#pizzerman').on('click', function(){
-    Clicker.clickUpgrade(upgrades[0]);
-    
-});
-
-$('#waitress').on('click', function(){
-    Clicker.clickUpgrade(upgrades[1]);
-    
-});
-
-$('#save').on('click', function(){
-    Clicker.save();
-    
-});
-
-$('#load').on('click', function(){
-    Clicker.load();
-    
-});
-
-$('#reset').on('click', function(){
-    Clicker.reset();
-    Clicker.load();
-    
+$(document).ready(function() {
+    var Clicker = new PizzaClickerGame().start();
 });
 
